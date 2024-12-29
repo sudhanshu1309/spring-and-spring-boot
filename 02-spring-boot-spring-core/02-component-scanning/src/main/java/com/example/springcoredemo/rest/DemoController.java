d0f6806fe2be1efc343550870e525d4402d07221
sudhanshu1309 <sudhanshutripathi008@gmail.com>
2024-12-30 00:14:23 +0530

    component scanning manually

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.springcoredemo.rest;
+
+import com.example.util.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    // define a get mapping
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/example/util/Coach.java
@@ -0,0 +1,5 @@
+package com.example.util;
+
+public interface Coach {
+    String getDailyWorkout();
+}
