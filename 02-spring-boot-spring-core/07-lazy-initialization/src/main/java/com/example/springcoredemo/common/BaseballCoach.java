333d718143964474a760ae08d74d85314b86368f
sudhanshu1309 <sudhanshutripathi008@gmail.com>
2024-12-30 01:10:41 +0530

    lazy-initialization

---
+++ 02-spring-boot-spring-core/07-lazy-initialization/src/main/java/com/example/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,18 @@
+package com.example.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class BaseballCoach implements Coach{
+
+    public BaseballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Baseball and Home run!";
+    }
+}
---
+++ 02-spring-boot-spring-core/07-lazy-initialization/src/main/java/com/example/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,16 @@
+package com.example.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice Practice and Practice";
+    }
+}
