712e7d5c96ce005ae126fe4dcb51eef1ac315fd3
sudhanshu1309 <sudhanshutripathi008@gmail.com>
2025-01-01 02:29:08 +0530

    implemented DAO for the Student entity

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.cruddemo;
 
+import com.example.cruddemo.dao.StudentDAO;
+import com.example.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,23 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating a new Student object...");
+		Student tempStudent = new Student("Sudhanshu", "Tripathi", "sudhanshu@gmail.com");
+
+		// save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display the id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }
---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
