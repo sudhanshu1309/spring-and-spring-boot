9313a7704361341eb4b39a145f9f418611808d88
sudhanshu1309 <sudhanshutripathi008@gmail.com>
2025-03-02 00:22:17 +0530

    GET and POST method in formData

--- 06-spring-boot-spring-mvc/01-thymeleaf-helloworld/src/main/java/com/example/thymeleaf/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleaf-helloworld/src/main/java/com/example/thymeleaf/controller/HelloWorldController.java
@@ -4,7 +4,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -38,4 +40,21 @@ public class HelloWorldController {
         // return the view
         return "helloworld";
     }
+
+    @PostMapping("/processformv3")
+    public String processFormV3(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        // return the view
+        return "helloworld";
+    }
 }
+
