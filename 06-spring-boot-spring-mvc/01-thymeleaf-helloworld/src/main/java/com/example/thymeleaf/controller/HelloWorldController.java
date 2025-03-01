ed268e8390c583e7d027b2b870b1ad1bd5ff6a3f
sudhanshu1309 <sudhanshutripathi008@gmail.com>
2025-03-01 23:55:17 +0530

    reading the formData

---
+++ 06-spring-boot-spring-mvc/01-thymeleaf-helloworld/src/main/java/com/example/thymeleaf/controller/HelloWorldController.java
@@ -0,0 +1,41 @@
+package com.example.thymeleaf.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    // need a controller method to show the initial HTML form
+    @GetMapping("/showform")
+    public String helloWorldForm() {
+        return "helloWorldForm";
+    }
+
+    // need a controller method to process the HTML form
+    @GetMapping("/processform")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    // need a controller method to read form data and add data to the model
+    @GetMapping("/processformv2")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        // return the view
+        return "helloworld";
+    }
+}
